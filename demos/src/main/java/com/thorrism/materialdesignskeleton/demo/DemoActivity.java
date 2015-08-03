f9a366a181302a7973eda744a266a46461b8a114
Lucas Crawford <lcrawford@hytrust.com>
2015-08-02 18:39:57 -0700

    Added more material design features to demo.

--- demos/src/main/java/com/thorrism/materialdesignskeleton/demo/DemoActivity.java
+++ demos/src/main/java/com/thorrism/materialdesignskeleton/demo/DemoActivity.java
@@ -1,7 +1,14 @@
 package com.thorrism.materialdesignskeleton.demo;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.thorrism.designtools.views.MaterialDialog;
@@ -12,14 +19,53 @@ import interfaces.FromXml;
 
 public class DemoActivity extends AppCompatActivity {
     private ShakeEditText mShakeEdit;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
+        //set toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Enable home button =
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeAsUpIndicator(R.mipmap.ic_menu);
+
+        //get drawer layout and navigation view
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavView = (NavigationView) findViewById(R.id.nav_view);
+        setupDrawerContent();
+
+        //Add shake edit and listener
         mShakeEdit = (ShakeEditText) findViewById(R.id.name_edit);
         mShakeEdit.setValidationListener(new EmailValidator(mShakeEdit));
+    }
+
+    private void setupDrawerContent() {
+        mNavView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @FromXml
